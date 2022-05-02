a3f75144e80e3d0ef4ae18da6aba36fd711ee224
Luna <44528100+pozm@users.noreply.github.com>
2022-05-02 20:18:17 +0100

    fix notifications css not being generated

--- src/Components/NotificationHandler.svelte
+++ src/Components/NotificationHandler.svelte
@@ -38,6 +38,7 @@ import xcircle from '../assets/svg/xcirico.svg'
 	})
 	// let notifiArr: IClientNotification[]=[]
 	// $: revNoti = [...$AppNotifications].reverse();
+	let usables = ["col-span-7","col-span-8"] // this makes tailwind actually generate the css.
 </script>
 <!-- {@debug notifications} -->
 <div class="absolute h-full w-72 py-2 right-2 z-60" >
