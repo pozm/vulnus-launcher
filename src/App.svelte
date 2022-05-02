96270bb85cd14a58561952265feb72d08c1fbb72
Luna <44528100+pozm@users.noreply.github.com>
2022-05-02 20:00:51 +0100

    change vulnus install path

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 
 <script lang="ts">
 	import Modal from './Components/Modal.svelte';
-import { dialog,fs,invoke } from "@tauri-apps/api";
+import { dialog,fs,invoke, path } from "@tauri-apps/api";
 
 import IndexPage from "./lib/pages/IndexPage.svelte";
 import { Data } from "./lib/store";
@@ -56,8 +56,41 @@ import InfoPage from './lib/pages/InfoPage.svelte';
 	]
 	let ShowPage = 0;
 
-	
-	
+	function GetVulnusPath() {
+		console.log("GetVulnusPath");
+		dialog.open({
+			directory:true,
+			defaultPath:updatePath,
+			title:"Select Vulnus path",
+			multiple:false
+		}).then(v=>{
+			updatePath = v as string;
+		})
+	}
+	function getDefaultPath() {
+		console.log("update")
+		updatePath = Data.Store.get.data.vulnus.path
+	}
+	function SetVulnusPath() {
+		invoke("set_path",{pathTo:updatePath}).then(()=>{
+			Data.Store.get.reload();
+			ShowPathModal.set(false);
+			updatePath = "";
+		})
+	}
+	$: {
+		invoke<boolean>("dir_exist",{dir: updatePath}).then(d=>{
+			PathIsInvalid=!d;
+			console.log(PathIsInvalid)
+		})
+		if ($ShowPathModal == true && usableget) {
+			usableget = false
+			getDefaultPath()
+		} else if ($ShowPathModal == false) {
+			usableget = true;
+		}
+	}
+	let usableget = true;
 	let SidebarHovering = false;
 	
 	// onMount(()=>{
@@ -76,10 +109,34 @@ import InfoPage from './lib/pages/InfoPage.svelte';
 	// onDestroy(()=>{
 	// 	// clearInterval(interval)
 	// })
-
+	
 </script>
 <div class="flex min-h-screen" >
 	<NotificationHandler/>
+	<Modal show={$ShowPathModal}>
+		<h1 class="text-gray-200 text-2xl" >Hi,</h1>
+		<h3 class="text-gray-300 text-lg">To change the location of the <span class="text-pink-300" >Vulnus Launcher</span> please enter a directory</h3>
+		<div class="relative">
+			<label for="password" class="block text-sm font-medium text-gray-400" >Please enter the parent of the desired path, we will make a folder for vulnus.</label>
+			<input on:blur={()=>{
+				PathActive=false;
+			}} on:focus={(v)=>{
+				PathActive=true
+			}} bind:value={updatePath} id="path" name="path" class={`appearance-none w-full text-neutral-200 placeholder-zinc-400 transition-colors bg-neutral-900 focus:outline-none focus:ring-pink-400 focus:border-pink-400 focus:ring-1 rounded-lg px-2 py-2 shadow-sm border ${PathIsInvalid ? "border-red-400 ring-red-400" : "border-zinc-600"}`} placeholder="Path of vulnus">
+			<button on:click={GetVulnusPath} class="absolute right-2 bottom-2.5 w-6 h-6" >
+				<svg xmlns="http://www.w3.org/2000/svg" class={`h-6 w-6 ${PathActive ? "text-pink-200" : "text-zinc-600"}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+				</svg>
+			</button>
+		</div>
+		<div class="flex justify-end" >
+			{#if PathIsInvalid}
+				<p class="mr-auto mt-2 text-sm text-red-400 select-none " >The path you have provided is is invalid </p>
+				
+			{/if}
+			<button disabled={PathIsInvalid} class="py-2 shadow-sm px-12 transition-colors hover:bg-green-600 text-gray-100 bg-emerald-500 disabled:bg-emerald-600/50 mt-2 rounded-lg" on:click="{SetVulnusPath}">Save</button>
+		</div>
+	</Modal>
 	<div class="min-h-screen select-none w-14 bg-zinc-800 flex flex-col space-y-2 py-2 hover:w-32 transition-all duration-200 items-center hover:items-start px-1" on:mouseleave={()=>SidebarHovering=false} on:mouseenter={()=>SidebarHovering=true} >
 		{#each PagesMap as page,i}
 			{@const selected = ShowPage == i}
--- src/lib/pages/SettingsPage.svelte
+++ src/lib/pages/SettingsPage.svelte
@@ -6,7 +6,7 @@ import { onMount } from "svelte";
 
 import { getDataDir, launcherDir } from "../SharedFunctions";
 import { Data } from "../store";
-import { VulnusPath } from "../StoreData";
+import { ShowPathModal, VulnusPath } from "../StoreData";
 
 
 
@@ -40,7 +40,7 @@ import { VulnusPath } from "../StoreData";
 			
 			<div class="flex flex-row w-full content-center items-center" >
 				<p class="text-gray-300 text-xl mr-auto" >Vulnus Path: <span class="text-pink-300" >{$VulnusPath}</span></p>
-				<button  class="ml-auto py-2 shadow-sm px-12 transition-colors hover:bg-cyan-600 text-gray-100 bg-cyan-500 disabled:bg-cyan-600/50 mt-2 rounded-lg">Change</button>
+				<button on:click={()=>{ShowPathModal.set(true)}}  class="ml-auto py-2 shadow-sm px-12 transition-colors hover:bg-cyan-600 text-gray-100 bg-cyan-500 disabled:bg-cyan-600/50 mt-2 rounded-lg">Change</button>
 			</div>
 		<div class="flex flex-row w-full content-center items-center" >
 			<p class="text-gray-300 text-xl mr-auto" >Open launcher folder</p>
